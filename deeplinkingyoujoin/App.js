72750a400ce764c77fe5c55db5d1e35e3b65d04a
Zach Odio <zachariaodio@gmail.com>
2022-07-03 20:20:08 -0600

    updates in packagejson

--- deeplinkingyoujoin/App.js
+++ deeplinkingyoujoin/App.js
@@ -1,14 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Linking from "expo-linking";
 export default function App() {
+
+  const [data, setData] = useState(null);
+
+
+  function handleDeepLink(event) {
+    let data = Linking.parse(event.url);
+    setData(data);
+  }
+  useEffect(() => {
+      async function getInitialURL() {
+        const initialUrl = await Linking.getInitialURL();
+        if (initialUrl) setData(Linking.parse(initialUrl));
+      }
+      
+    Linking.addEventListener("url", handleDeepLink);
+    if (!data) {
+      getInitialURL();
+    }
+
+    return () => {  
+      Linking.removeEventListener("url");
+    };
+
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        {data ? JSON.stringify(data) : "DeepLink Navigation Youjoin"}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
